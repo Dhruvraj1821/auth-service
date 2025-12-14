95810fb3c0003072e4f047aa3aef31af538f6471
Dhruvraj1821 <dhruvraj18211821@gmail.com>
2025-12-14 19:31:40 +0530

    login implemented

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,8 +1,17 @@
 import { Router } from "express";
-import {registerUser} from "../controllers/auth.controller.js";
+import {registerUser, loginUser} from "../controllers/auth.controller.js";
+import {protect} from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.post("/register",registerUser);
+router.post("/login", loginUser);
+
+router.get("/me", protect, (req, res) => {
+  res.status(200).json({
+    message: "Access granted",
+    userId: req.userId,
+  });
+});
 
 export default router;
