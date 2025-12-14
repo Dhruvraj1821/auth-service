f0b1b3ed3e2c73812b6d5005857d108da5b6ca19
Dhruvraj1821 <dhruvraj18211821@gmail.com>
2025-12-14 23:11:22 +0530

    implemented access and refresh token for more secure api access

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import express from "express";
-
+import cookieParser from "cookie-parser";
 import healthRouter from "./routes/healthRoute.js";
 import authRouter from "./routes/auth.route.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/health", healthRouter);
 app.use("/auth",authRouter);
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import {registerUser, loginUser} from "../controllers/auth.controller.js";
+import {registerUser, loginUser,logoutUser,refreshAccessToken} from "../controllers/auth.controller.js";
 import {protect} from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.post("/register",registerUser);
 router.post("/login", loginUser);
+router.post("/refresh",refreshAccessToken);
+router.post("/logout",logoutUser);
 
 router.get("/me", protect, (req, res) => {
   res.status(200).json({
