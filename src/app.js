2a6fe0d9995bbd971fcf340e2b70c83983ad43bd
Dhruvraj1821 <dhruvraj18211821@gmail.com>
2025-12-14 18:17:33 +0530

    signup implemented

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 
 import healthRouter from "./routes/healthRoute.js";
+import authRouter from "./routes/auth.route.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/health", healthRouter);
+app.use("/auth",authRouter);
 
 export default app;
